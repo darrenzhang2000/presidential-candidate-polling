6a94fe4f78a66c3de94026d5357c05a294e4e483
darren <darrenzhang200027@gmail.com>
2020-08-28 12:48:57 -0400

    retrieved date, pollster name, sample size, net, and percent favorable for each row

--- app.py
+++ app.py
@@ -23,20 +23,31 @@ driver.get('https://projects.fivethirtyeight.com/polls/president-general/')
 
 rows = driver.find_elements_by_class_name('visible-row')
 for r in rows:
-    # note that element is singular because each tr only has one date
+    # Note that element is singular 
     date = r.find_element_by_class_name("date-wrapper").text
-    print(date, '\n')
 
+    # The pollster container can have up to two links. One for the grade, if it exists,
+    # and one for the pollster name. We want the latter.
+    pollster_container = r.find_element_by_class_name("pollster-container")
+    pollster_links = pollster_container.find_elements_by_tag_name("a")
+    pollster_name = pollster_links[-1].text # Accesses last element of the link array
+    
+    sample_size = r.find_element_by_class_name("sample").text
+    
+    leader = r.find_element_by_class_name("leader").text
 
-# pollster_links = driver.find_elements_by_class_name("pollster-container")
+    net = r.find_element_by_class_name("net").text
 
-# for p in pollster_links:
-#     #get child link
-#     link = p.find_elements_by_tag_name("a")[1]
-#     pollster = link.text
-#     print(pollster, '\n')
-# #     #great! at this point we've created a bunch of
-# print(driver.page_source)
+    values = r.find_elements_by_class_name("value")
 
-# print(pollster_links)
+    # If the other value is hidden by the "more" button
+    if len(values) == 1:   
+        next_sibling = r.find_element_by_xpath("following-sibling::tr[@class='expandable-row']")
+        value = next_sibling.find_element_by_class_name("value")
+        values.append(value)
+    
+    # For some reason using .text doesn't always work, so I'm using .get_attribute instead
+    trump_fav = values[0].find_element_by_class_name("heat-map").get_attribute('innerHTML')
+    biden_fav = values[1].find_element_by_class_name("heat-map").get_attribute('innerHTML')
+
 driver.quit()
